15a145e4c2690073ab96ece039acc457ba83890e
JohannesSchaefers <info@iconicity.de>
2024-02-11 14:54:55 +0100

    df

--- islands/restcounter.tsx
+++ islands/restcounter.tsx
@@ -1,10 +1,10 @@
-
 // programmed by ChatGPT
 
 //import { h, FunctionalComponent } from 'preact';
 //import { useEffect } from 'preact/hooks';
 
 // Importing Preact
+// Here we import Preact's implementation of the h function, and also a render function
 
 import { h, render, FunctionalComponent } from 'https://esm.sh/preact@10.19.3';
 
@@ -15,13 +15,13 @@ interface CounterProps {
   count: { value: number };
 }
 
-const Counter1: FunctionalComponent<CounterProps> = (props: CounterProps) => {
+const Counter1: FunctionalComponent<CounterProps> = ( props: CounterProps) => {
   // Assuming there's a variable named `valueToUpdate` that you want to watch for changes
   let valueToUpdate = 8;
 
   useEffect(() => {
     // Update props.count.value when the variable valueToUpdate changes
-    props.count.value = valueToUpdate;
+    props.count.value = valueToUpdate;  
   }, [valueToUpdate]);
 
   return (
